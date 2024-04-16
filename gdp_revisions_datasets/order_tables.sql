e64fccf11ee55a4d1b014aef5e0a4b18c8f1f201
Jason18 <164468@unsaac.edu.pe>
2024-04-15 23:42:08 -0500

    Added code to order by year, date and id_ns for all frecuencies (m, q and a)

--- gdp_revisions_datasets/order_tables.sql
+++ gdp_revisions_datasets/order_tables.sql
@@ -1,14 +1,96 @@
---------------------------
+--******************************
 -- ORDER TABLE
---------------------------
+--******************************
 
-CREATE TABLE gdp_monthly_growth_rates_sorted AS
-SELECT * FROM gdp_monthly_growth_rates
-ORDER BY year, id_ns, date;
+--CREATE TABLE construction_monthly_growth_rates_sorted AS
+--SELECT * FROM construction_monthly_growth_rates
+--ORDER BY year, id_ns, date;
 
-DROP TABLE IF EXISTS "gdp_monthly_growth_rates";
+--DROP TABLE IF EXISTS "construction_monthly_growth_rates";
 
-ALTER TABLE gdp_monthly_growth_rates_sorted RENAME TO gdp_monthly_growth_rates;
+--ALTER TABLE construction_monthly_growth_rates_sorted RENAME TO construction_monthly_growth_rates;
 
-SELECT * FROM gdp_monthly_growth_rates;
+--SELECT * FROM construction_monthly_growth_rates;
 
+--++++++++++++++++++++++++++++++
+-- MONTHLY
+--++++++++++++++++++++++++++++++
+
+DO $$
+DECLARE
+    tbl RECORD;
+BEGIN
+    FOR tbl IN 
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_name LIKE '%_monthly_growth_rates'
+    LOOP
+        EXECUTE format('
+            CREATE TABLE %I_sorted AS
+            SELECT * FROM %I
+            ORDER BY year, id_ns, date;
+
+            DROP TABLE IF EXISTS %I;
+
+            ALTER TABLE %I_sorted RENAME TO %I;
+        ', tbl.table_name, tbl.table_name, tbl.table_name, tbl.table_name, tbl.table_name);
+    END LOOP;
+END $$;
+
+
+--++++++++++++++++++++++++++++++
+-- QUARTERLY
+--++++++++++++++++++++++++++++++
+
+DO $$
+DECLARE
+    tbl RECORD;
+BEGIN
+    FOR tbl IN 
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_name LIKE '%_quarterly_growth_rates'
+    LOOP
+        EXECUTE format('
+            CREATE TABLE %I_sorted AS
+            SELECT * FROM %I
+            ORDER BY year, id_ns, date;
+
+            DROP TABLE IF EXISTS %I;
+
+            ALTER TABLE %I_sorted RENAME TO %I;
+        ', tbl.table_name, tbl.table_name, tbl.table_name, tbl.table_name, tbl.table_name);
+    END LOOP;
+END $$;
+
+
+--++++++++++++++++++++++++++++++
+-- ANNUAL
+--++++++++++++++++++++++++++++++
+
+DO $$
+DECLARE
+    tbl RECORD;
+BEGIN
+    FOR tbl IN 
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_name LIKE '%_annual_growth_rates'
+    LOOP
+        EXECUTE format('
+            CREATE TABLE %I_sorted AS
+            SELECT * FROM %I
+            ORDER BY year, id_ns, date;
+
+            DROP TABLE IF EXISTS %I;
+
+            ALTER TABLE %I_sorted RENAME TO %I;
+        ', tbl.table_name, tbl.table_name, tbl.table_name, tbl.table_name, tbl.table_name);
+    END LOOP;
+END $$;
+
+--
+-- SELECT
+--
+
+SELECT * FROM "gdp_monthly_growth_rates";
