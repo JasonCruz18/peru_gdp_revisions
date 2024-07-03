e2d49b204881daec131fc18144dffb3b1433b29b
Jason18 <164468@unsaac.edu.pe>
2024-07-02 23:00:28 -0500

    Fixed random_wait function

--- gdp_revisions_datasets/gdp_revisions_datasets_functions.py
+++ gdp_revisions_datasets/gdp_revisions_datasets_functions.py
@@ -16,9 +16,9 @@ def play_sound():
     
 # Function to wait random seconds
 def random_wait(min_time, max_time):
-wait_time = random.uniform(min_time, max_time)
-print(f"Waiting randomly for {wait_time:.2f} seconds")
-time.sleep(wait_time)
+    wait_time = random.uniform(min_time, max_time)
+    print(f"Waiting randomly for {wait_time:.2f} seconds")
+    time.sleep(wait_time)
 
 
 # Function to download PDFs
