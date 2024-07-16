31f2bae49abd5a1e206506a164e89fc6f0b677d2
Jason18 <164468@unsaac.edu.pe>
2024-07-16 02:45:37 -0500

    Added functionss to clean-up the data delivered by Central Bank

--- gdp_revisions_datasets/old_gdp_revisions_datasets_functions.py
+++ gdp_revisions_datasets/old_gdp_revisions_datasets_functions.py
@@ -71,3 +71,61 @@ def duplicate_files(year, df_year, base_path):
                 print(f"Duplicated {last_existing_file} to {new_file_name}")
             else:
                 print(f"No existing file to duplicate for {new_file_name}")
+
+
+
+################################################################################################
+# Section 2. Data cleaning
+################################################################################################
+                
+
+# Function to ... PENDING
+# _________________________________________________________________________
+
+
+#**********************************************************************************************
+# Section 2.1.  Extracting tables and data cleanup 
+#----------------------------------------------------------------------------------------------
+
+#+++++++++++++++
+# LIBRARIES
+#+++++++++++++++
+
+
+
+
+
+#...............................................................................................
+#...............................................................................................
+# Functions for tables delivered by Central Bank
+# ______________________________________________________________________________________________
+#...............................................................................................
+
+# 1. Convert column names to lowercase and remove accents
+def clean_column_names(df):
+    # Convert column names to lowercase
+    df.columns = df.columns.str.lower()
+    # Normalize string column names to remove accents
+    df.columns = [unicodedata.normalize('NFKD', col).encode('ASCII', 'ignore').decode('utf-8') if isinstance(col, str) else col for col in df.columns]
+    return df
+
+# 2. Adjust column names
+def adjust_column_names(df):
+    # Check if the first observation in the first column is NaN
+    if pd.isna(df.iloc[0, 0]) and pd.isna(df.iloc[0, -1]):
+        # Verify column names
+        if "sectores economicos" in df.columns[0] and "economic sectors" in df.columns[-1]:
+            # Replace NaN with corresponding column names
+            df.iloc[0, 0] = "sectores economicos"
+            df.iloc[0, -1] = "economic sectors"
+    return df
+
+# 3. Round values in DataFrame columns
+def rounding_values(df, decimals=1):
+    # Iterate over all columns in the DataFrame
+    for col in df.columns:
+        # Check if the column is of type float
+        if df[col].dtype == 'float64':
+            # Round the values in the column to the specified number of decimals
+            df[col] = df[col].round(decimals)
+    return df
